aae44e4cb4e94bf343ea38f3be6861962902c33f
Adi's Mac <Mac@mac.local>
2021-07-24 22:36:13 -0700

    Added a title to src/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-//*********************THIS IS THE CLIENT SIDE OF THE APP */
+//*********************THIS IS THE CLIENT SIDE OF THE APP *************/
 console.log('Client side javascript file is loaded!')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
